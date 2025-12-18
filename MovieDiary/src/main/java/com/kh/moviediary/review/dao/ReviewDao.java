a2b84700b5ebcf3a6a2e893402329cfca39088f7
minchaeee514 <zena514@naver.com>
2025-12-18 17:54:40 +0900

    dao- repository 진행중

--- MovieDiary/src/main/java/com/kh/moviediary/review/dao/ReviewDao.java
+++ MovieDiary/src/main/java/com/kh/moviediary/review/dao/ReviewDao.java
@@ -1,9 +1,11 @@
 package com.kh.moviediary.review.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.kh.moviediary.review.model.vo.Review;
 
+@Repository
 public class ReviewDao {
 
 	public int reviewInsert(Review r, SqlSessionTemplate sqlSession) {
