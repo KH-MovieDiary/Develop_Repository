0c204802b8f35e1703d4dee41c58f414a892a772
jsi4770 <chosi4770@gmail.com>
2025-12-30 17:37:58 +0900

    noteHandler

---
+++ MovieDiary/src/main/java/com/kh/moviediary/websocket/server/NoteHandler.java
@@ -0,0 +1,10 @@
+package com.kh.moviediary.websocket.server;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class NoteHandler extends TextWebSocketHandler {
+	
+	
+	
+	
+}
